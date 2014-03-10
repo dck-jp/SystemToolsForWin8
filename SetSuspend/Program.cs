ef402970a3c81e5ce00a4fceb30d90918a920369
D*isuke YAMAKAWA <dicek.am@clockahead.com>
2014-03-10 21:05:27 +0900

    First Release

---
+++ SetSuspend/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Suspend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Application.SetSuspendState(PowerState.Suspend, false, false);
+        }
+    }
+}
