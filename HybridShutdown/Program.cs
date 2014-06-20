5fc16f2c161a86ce6a3f8d7b8dbe37e7aa53d021
dck-jp <dicek.am@clockahead.com>
2014-06-21 00:29:35 +0900

    Refactoring & Shutdown.exe use ExitWindowsEx EWX_POWEROFF  without EWX_FORCE

--- HybridShutdown/Program.cs
+++ HybridShutdown/Program.cs
@@ -8,114 +8,13 @@ namespace HybridShutdown
 {
     class Program
     {
-        public enum ExitWindows : uint
-        {
-            EWX_HYBRID_SHUTDOWN = 0x00400000,
-            EWX_LOGOFF = 0x00,
-            EWX_SHUTDOWN = 0x01,
-            EWX_REBOOT = 0x02,
-            EWX_POWEROFF = 0x08,
-            EWX_RESTARTAPPS = 0x40,
-
-            EWX_FORCE = 0x04,
-            EWX_FORCEIFHUNG = 0x10,
-        }
-
-        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
-        public static extern bool ExitWindowsEx(ExitWindows uFlags,
-            int dwReason);
-
-        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
-        private static extern IntPtr GetCurrentProcess();
-
-        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool OpenProcessToken(IntPtr ProcessHandle,
-            uint DesiredAccess,
-            out IntPtr TokenHandle);
-
-        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true,
-            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
-        private static extern bool LookupPrivilegeValue(string lpSystemName,
-            string lpName,
-            out long lpLuid);
-
-        [System.Runtime.InteropServices.StructLayout(
-           System.Runtime.InteropServices.LayoutKind.Sequential, Pack = 1)]
-        private struct TOKEN_PRIVILEGES
-        {
-            public int PrivilegeCount;
-            public long Luid;
-            public int Attributes;
-        }
-
-        [System.Runtime.InteropServices.DllImport("advapi32.dll", SetLastError = true)]
-        private static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
-            bool DisableAllPrivileges,
-            ref TOKEN_PRIVILEGES NewState,
-            int BufferLength,
-            IntPtr PreviousState,
-            IntPtr ReturnLength);
-
-        public static void AdjustToken()
-        {
-            const uint TOKEN_ADJUST_PRIVILEGES = 0x20;
-            const uint TOKEN_QUERY = 0x8;
-            const int SE_PRIVILEGE_ENABLED = 0x2;
-            const string SE_SHUTDOWN_NAME = "SeShutdownPrivilege";
-
-            if (Environment.OSVersion.Platform != PlatformID.Win32NT)
-                return;
-
-            IntPtr procHandle = GetCurrentProcess();
-
-            //トークンを取得する
-            IntPtr tokenHandle;
-            OpenProcessToken(procHandle,
-                TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out tokenHandle);
-            //LUIDを取得する
-            TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES();
-            tp.Attributes = SE_PRIVILEGE_ENABLED;
-            tp.PrivilegeCount = 1;
-            LookupPrivilegeValue(null, SE_SHUTDOWN_NAME, out tp.Luid);
-            //特権を有効にする
-            AdjustTokenPrivileges(
-                tokenHandle, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
-        }
-
-        [DllImport("kernel32.dll")]
-        static extern uint FormatMessage(
-          uint dwFlags, IntPtr lpSource,
-          uint dwMessageId, uint dwLanguageId,
-          StringBuilder lpBuffer, int nSize,
-          IntPtr Arguments);
-        private const uint FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
-
         static void Main(string[] args)
         {
-            AdjustToken();
-            WriteDebugFile(Marshal.GetLastWin32Error());
+            Util.AdjustToken();
+            Util.WriteDebugFile(Marshal.GetLastWin32Error());
 
-            ExitWindowsEx(ExitWindows.EWX_HYBRID_SHUTDOWN | ExitWindows.EWX_SHUTDOWN, 0);
-            WriteDebugFile(Marshal.GetLastWin32Error());
-        }
-
-        private static void WriteDebugFile(int errorNumber)
-        {
-            if (errorNumber == 0) return;
-
-            StringBuilder message = new StringBuilder(255);
-            var errorMessage = FormatMessage(
-              FORMAT_MESSAGE_FROM_SYSTEM,
-              IntPtr.Zero,
-              (uint)errorNumber,
-              0,
-              message,
-              message.Capacity,
-              IntPtr.Zero);
-            var path = Environment.SpecialFolder.Desktop + @"\debug.log";
-
-            File.WriteAllText(path,
-                string.Format(@"%0 \t %1 : %2", DateTime.Now, errorNumber, errorMessage));
+            Win32API.ExitWindowsEx(Win32API.ExitWindows.EWX_HYBRID_SHUTDOWN | Win32API.ExitWindows.EWX_SHUTDOWN, 0);
+            Util.WriteDebugFile(Marshal.GetLastWin32Error());
         }
     }
 }
---
+++ Shutdown/Win32API.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HybridShutdown
+{
+    static class Win32API
+    {
+        public enum ExitWindows : uint
+        {
+            EWX_HYBRID_SHUTDOWN = 0x00400000,
+            EWX_LOGOFF = 0x00,
+            EWX_SHUTDOWN = 0x01,
+            EWX_REBOOT = 0x02,
+            EWX_POWEROFF = 0x08,
+            EWX_RESTARTAPPS = 0x40,
+
+            EWX_FORCE = 0x04,
+            EWX_FORCEIFHUNG = 0x10,
+        }
+
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool ExitWindowsEx(ExitWindows uFlags, int dwReason);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint FormatMessage(
+          uint dwFlags, IntPtr lpSource,
+          uint dwMessageId, uint dwLanguageId,
+          StringBuilder lpBuffer, int nSize,
+          IntPtr Arguments);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetCurrentProcess();
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool OpenProcessToken(IntPtr ProcessHandle,
+            uint DesiredAccess,
+            out IntPtr TokenHandle);
+
+        [DllImport("advapi32.dll", SetLastError = true,
+            CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+        public static extern bool LookupPrivilegeValue(string lpSystemName,
+            string lpName,
+            out long lpLuid);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 1)]
+        public struct TOKEN_PRIVILEGES
+        {
+            public int PrivilegeCount;
+            public long Luid;
+            public int Attributes;
+        }
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle,
+            bool DisableAllPrivileges,
+            ref TOKEN_PRIVILEGES NewState,
+            int BufferLength,
+            IntPtr PreviousState,
+            IntPtr ReturnLength);
+    }
+}
