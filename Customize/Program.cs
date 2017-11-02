ad35b77a5d1a30be30f495103133def93d5afb3e
dck-jp <dicek.am@clockahead.com>
2017-11-02 12:02:57 +0900

    Windows8用のロック画面スキップ設定を行うプログラム追加

---
+++ Customize/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Customize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ロック画面を表示しないようにします。");
+            Console.WriteLine("「サインイン時または画面上のすべてのアプリを終了したときに、スタート画面ではなくデスクトップに移動する」をONにします。");
+            Console.WriteLine("Press Any Key..");
+            Console.Read();
+
+            using (var reg = Registry.LocalMachine.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows\Personalization"))
+            {
+                reg.SetValue("NoLockScreen", 1);                
+            }
+
+            using (var reg = Registry.LocalMachine.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Explorer\StartPage"))
+            {
+                reg.SetValue("OpenAtLogon", 0);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
